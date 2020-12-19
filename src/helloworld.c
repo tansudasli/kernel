f7b8cb0996d5b613eff71b9e578b74e4bd1eab55
tansudasli <tansudasli@gmail.com>
2020-12-20 02:30:35 +0300

    added parameter usage

--- src/helloworld.c
+++ src/helloworld.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
-// run with, gcc -o out/helloworld helloworld.c
+
+// build & run with, 
+//     gcc -o out/helloworld src/helloworld.c 
+//     ./out/helloworld ali veli deli
+
+//char **argv == char *argv[]
 int main(int argc, char const *argv[])
 {
     /* code */
     printf("hello World\n");
+    puts("hi, again");
+
+    //access 2 parameters
+    for (size_t i = 0; i < argc; i++)
+    {
+        /* code */
+        printf("%zu, argc=%d, %s \n", i, argc, argv[i]);
+    }
+    
     return 0;
 }
