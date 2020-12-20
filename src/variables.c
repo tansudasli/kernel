3614eb73e1563985808a52012b34d3399b30f76b
tansudasli <tansudasli@gmail.com>
2020-12-20 13:42:13 +0300

    Initialzie - basic variable concepts

---
+++ src/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define TAX_STANDART 0.18
+
+/**
+ * Build, then Run w/
+ *    gcc -o out/variables src/variables.c && ./out/variables 
+ */
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("TAX_STANDART = %f \n", TAX_STANDART);
+
+    return 0;
+}
