30d3fde4c442d7009cd58529a28d28f081a26b10
tansudasli <tansudasli@gmail.com>
2020-12-20 13:50:07 +0300

    const vs #define

--- src/variables.c
+++ src/variables.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+//#define (you may still reassign :)) or const (real constant). 
 #define TAX_STANDART 0.18
+const double TAX_LUX_CONSUPTION=0.20;
 
 /**
  * Build, then Run w/
@@ -9,7 +11,13 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("TAX_STANDART = %f \n", TAX_STANDART);
+    printf("TAX_STANDART = %f \n", TAX_STANDART);      //0.180000, 8 precision digit
+    printf("TAX_STANDART = %.3f \n", TAX_STANDART);    //0.180
+    printf("TAX_LUX_CONSUPTION = %.3f \n", TAX_LUX_CONSUPTION);    //0.200
+
+    //w/o casting it gives crazy something :)
+    printf("TAX_STANDART = %d \n", (int)TAX_STANDART); //0
+
 
     return 0;
 }
