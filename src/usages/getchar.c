2a313dc876e7e58ee5a37fb2624e2d822ec01157
tansudasli <tansudasli@gmail.com>
2020-12-21 18:59:53 +0300

    getchar manuplations

---
+++ src/usages/getchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//gcc -o out/getchar src/usages/getchar.c  && out/getchar
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char string[5];
+
+    int ch = getchar();   //reads and returns numeric value of char,  10 for a
+
+    //in a normal scenario, use for and increment string value makes it a word!
+    string[0] = ch;       //sets as char, a
+
+    printf("ch=%d, %s", ch, string);
+
+    return 0;
+}
