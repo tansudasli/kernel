0598019e55ae31726f4e11931d83bfbafd3d05bd
tansudasli <tansudasli@gmail.com>
2020-12-31 19:24:31 +0300

    renamed

---
+++ ansi-c/src/manipulationsString.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h> 
+
+#define MAX_STRING_LENGTH 200
+
+// gcc -o out/manipulationsString src/manipulationsString.c  && out/manipulationsString
+int main(int argc, char const *argv[]) {
+
+
+    char charArray[] = "world";   //char charArray[] = {'w', 'o', 'r', 'd'};
+                                  //charArray[0] = 'h'; charArray[1] = 'e' ...
+    char *string;                 //*name = "world";
+
+
+    char *stringArray[] = {"Abidin", "Denyo", "Veli", "Deli"};
+
+    //todo: implement string array manipulations
+
+    return 0;
+}
