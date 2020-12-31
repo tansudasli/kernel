dce1bf9565b34c3fb8e8c4075ca7a03433fad107
tansudasli <tansudasli@gmail.com>
2021-01-01 00:31:00 +0300

    search in char arrays added

--- ansi-c/src/manipulationsString.c
+++ ansi-c/src/manipulationsString.c
@@ -3,18 +3,36 @@
 
 #define MAX_STRING_LENGTH 200
 
+//    char *string;
+char sentence[] = "On the dark night, Fallow the light and Embrace the shadow which will lead you the way.";
+char tens[]     = "        10        20        30        40        50        60   65";
+char nums[]     = "123456789012345678901234567890123456789012345678901234567890123456";
+
+void printString() {
+    printf("%s\n%s \n%s", sentence, tens, nums);
+}
+
+void searchString(char source[], char searchString[]) {
+
+    char *foundAt = strstr(source, searchString);
+    int index = (int)(foundAt - source);
+
+    foundAt != NULL ? printf("%s, %d ... %d => %d\n", foundAt, source, foundAt, index) : printf("Not found");
+
+}
+
 // gcc -o out/manipulationsString src/manipulationsString.c  && out/manipulationsString
 int main(int argc, char const *argv[]) {
 
+    printf("%lu \t%lu\n", strlen(sentence), strlen(tens));
 
-    char charArray[] = "world";   //char charArray[] = {'w', 'o', 'r', 'd'};
-                                  //charArray[0] = 'h'; charArray[1] = 'e' ...
-    char *string;                 //*name = "world";
+    printString(); printf("\n");
 
+    printf("%c\n", sentence[65]);
+    searchString(sentence, "will");
 
-    char *stringArray[] = {"Abidin", "Denyo", "Veli", "Deli"};
-
-    //todo: implement string array manipulations
+    printf("%c\n", sentence[12]);
+    searchString(sentence, "night");
 
     return 0;
 }
