9f71d615013a57fc764ca06db19e9798dbc48b02
tansudasli <tansudasli@gmail.com>
2021-01-01 18:13:55 +0300

    headers files added

--- ansi-c/src/structs.c
+++ ansi-c/src/structs.c
@@ -1,34 +1,7 @@
 #include <stdio.h>
-#include <string.h>
+#include "header/structs.h"
 
-#define NUMBER_OF_CDS 2
-
-typedef enum score {
-    TERRIBLE, BAD, AVERAGE, GOOD, EXCELLENT
-} SCORE;
-
-typedef struct cd {
-    char title[50];
-    char artist[50];
-    int trackCount;
-    SCORE rating;
-} CD; //So we can use like native int/char type...
-
-CD cdDataset[NUMBER_OF_CDS];
-
-void initializeDataset() {
-    strcpy(cdDataset[0].title, "Once upon a time");
-    strcpy(cdDataset[0].artist, "Ali Veli");
-    cdDataset[0].trackCount = 10;
-    cdDataset->rating = TERRIBLE;
-
-    strcpy(cdDataset[1].title, "Just run forest");
-    strcpy(cdDataset[1].artist, "Mali Dali");
-    cdDataset[1].trackCount = 1;
-    cdDataset->rating = AVERAGE;
-}
-
-// gcc -o out/structs src/structs.c  && out/structs
+//  gcc src/structs.c src/utility/initialize.c -o out/structs  && out/structs
 int main(int argc, char **argv) {
 
     initializeDataset();
