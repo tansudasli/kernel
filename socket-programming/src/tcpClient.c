de4cb9a7700cc3f9f6939bc2b3f1882a62ad3be2
tansudasli <tansudasli@gmail.com>
2021-01-03 13:36:08 +0300

    renamed

--- socket-programming/src/tcpClient.c
+++ socket-programming/src/tcpClient.c
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[]) {
     serverAddress.sin_port = (argv[1] != NULL) ? htons(atoi(argv[1])) : htons(PORT);
     serverAddress.sin_addr.s_addr = INADDR_ANY; //0.0.0.0
 
-    connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    connect(clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
 
     //
     recv(clientSocket, receiveMessage, sizeof(receiveMessage), 0);
