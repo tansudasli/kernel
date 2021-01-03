90da4eef4c9a8beb627f3dcf32b34fba64753f66
tansudasli <tansudasli@gmail.com>
2021-01-03 20:36:54 +0300

    Initialize

---
+++ socket-programming/src/internal/client.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+/**
+ *
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char const *argv[]) {
+
+
+
+}
