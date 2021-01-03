b62321ae3a04390c38b5d68524d95640b4165d33
tansudasli <tansudasli@gmail.com>
2021-01-03 12:56:55 +0300

    basic tcp server & client - on same machine

--- socket-programming/src/tcp_client.c
+++ socket-programming/src/tcp_client.c
@@ -5,7 +5,41 @@
 
 #include <netinet/in.h>
 
+#define PORT 9000
+#define MAX_QUEUE_LENGTH 2
+#define TEST "Test"
+
+char requestMessage[256] = TEST;
+char receiveMessage[256];
+
+/**
+ * Basically, It creates a socket(), and connect() to server's master.
+ * Then starts receive() and send() data to the client handle created by master.
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char const *argv[]) {
+
+    //create the socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);  //domain, type, protocol
+
+    if (clientSocket == -1)
+        printf("Socket not created: %d", clientSocket);
+
+    //define address
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = (argv[1] != NULL) ? htons(atoi(argv[1])) : htons(PORT);
+    serverAddress.sin_addr.s_addr = INADDR_ANY; //0.0.0.0
+
+    connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+    //
+    recv(clientSocket, receiveMessage, sizeof(receiveMessage), 0);
+
+    printf("received=%s", receiveMessage);
 
 }
 
