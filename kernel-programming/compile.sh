a1f30bd1a5e0b46f1af7ce2609d32bbd77018fe5
tansudasli <tansudasli@gmail.com>
2021-01-06 01:14:09 +0300

    Added install and check kernel module steps

--- kernel-programming/compile.sh
+++ kernel-programming/compile.sh
@@ -4,3 +4,12 @@ make -C /lib/modules/`uname -r`/build M=${PWD} modules
 
 # to clean output files
 #make -C /lib/modules/`uname -r`/build M=${PWD} clean
+
+#list modinfo
+modinfo helloworld.ko
+
+#install kernel module
+sudo insmod helloworld.ko
+
+#be sure that u see at top your new module
+lsmod | less
