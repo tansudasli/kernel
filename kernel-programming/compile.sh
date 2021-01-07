f3edf84303484a4ba28b89e35bdf08d07daa31fa
tansudasli <tansudasli@gmail.com>
2021-01-07 16:01:20 +0300

    bugfix: sudo privilege added to rmmod

--- kernel-programming/compile.sh
+++ kernel-programming/compile.sh
@@ -5,7 +5,7 @@ make -C /lib/modules/`uname -r`/build M=${PWD} modules
 # to clean output files
 #make -C /lib/modules/`uname -r`/build M=${PWD} clean
 
-#list modinfo
+#get details
 modinfo helloworld.ko
 
 #install kernel module
@@ -14,8 +14,8 @@ sudo insmod helloworld.ko
 #be sure that u see at top your new module
 lsmod | less
 
+#remove module
+sudo rmmod helloworld.ko
+
 #check init and exit
 dmesg
-
-#remove module
-rmmod helloworld.ko
