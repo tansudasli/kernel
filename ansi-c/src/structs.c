6106246dbd96c98c04e78c946f04582587755f5f
tansudasli <tansudasli@gmail.com>
2021-01-11 16:58:10 +0300

    added accessing by pointer 2 struct

--- ansi-c/src/structs.c
+++ ansi-c/src/structs.c
@@ -7,17 +7,26 @@ int main(int argc, char **argv) {
     initializeDataset();
 
     //print dataset
-    CD this;
     printf("Title \t\t\t| Rating\n");
+
+    CD this;
     for (int i = 0; i < NUMBER_OF_CDS; i++) {
         this = cdDataset[i];
 
-        printf("%s \t| %u  \t| %p \n", this.title, this.rating, (struct CD *)&cdDataset[i]);
-
+        printf("%s \t| %u  \t| %p \n", this.title, this.rating, (CD *)&cdDataset[i]);
     }
 
+    printf("-------Access w/ pointer--------\n");
+    CD *that = cdDataset;
+    for (int i = 0; i < NUMBER_OF_CDS; i++) {
+
+        printf("%s \t| %u  \t| %p \n", that->title, that->rating, that);
+        that++;
+    }
+
+    printf("------------------\n");
     printf("size of CD=%lu\n", sizeof(cdDataset));
-    printf("size for first pointer CD=%lu", sizeof(cdDataset)/NUMBER_OF_CDS);
+    printf("size for first pointer CD=%lu\n", sizeof(cdDataset)/NUMBER_OF_CDS);
 
     return 0;
 }
