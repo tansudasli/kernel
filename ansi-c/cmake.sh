91c6378eb763a6261e016ae165b5b61111674c0e
tansudasli <tansudasli@gmail.com>
2021-01-12 18:12:58 +0300

    compile cmd w/ cmake

--- ansi-c/cmake.sh
+++ ansi-c/cmake.sh
@@ -1,3 +1,7 @@
+#on linux/Mac/windows
+#under ansi-c folder
+cmake . -B out
+make -C out
 
-
-cmake --build cmake-build-debug --target all -- -j 30
+#on IDE - clion
+#cmake --build cmake-build-debug --target all -- -j 30
