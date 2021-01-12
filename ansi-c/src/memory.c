10d767ecdc0f04a1965eb8203989ad4216ee4866
tansudasli <tansudasli@gmail.com>
2021-01-12 17:25:51 +0300

    calloc

---
+++ ansi-c/src/memory.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LENGTH 3
+
+int main (int argc, char **argv) {
+    int i;
+    int n = !argv[1] ?  3 : atoi(argv[1]);
+    int *a = (int*)calloc(1, sizeof(int));  //to initialize a pointer, use either &int[], or malloc/calloc !!
+                                                  //count is not important :)
+                                                  //initialize w/ 0
+
+//    printf("Enter %d numbers:\n",n);
+//    for( i=0 ; i < n ; i++ ) {
+//        scanf("%d", &a[i]);
+//    }
+
+    printf("The numbers entered are: ");
+    for( i=0 ; i < n ; i++ ) {
+        printf("%d ", a[i]);
+    }
+    free( a );
+
+    return(0);
+}
