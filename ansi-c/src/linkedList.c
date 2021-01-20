aad935c9969d4cd980882e1cebb1d8cb73f4bd9d
tansudasli <tansudasli@gmail.com>
2021-01-21 00:51:37 +0300

    minor update

--- ansi-c/src/linkedList.c
+++ ansi-c/src/linkedList.c
@@ -23,14 +23,14 @@ int main (int argc, char **argv) {
     //list all nodes, from 1st node
     display(getFirst());
 
-    printf("\n1st => %d %p", getFirst()->info, getFirst()->link);
-    printf("\nNth => %d %p", getLast()->info, getLast()->link);
+    printf("\n1st  => %d %p", getFirst()->info, getFirst()->link);
+    printf("\nlast => %d %p", getLast()->info, getLast()->link);
     printf("\nnodes (display)= %d \n", count());
 
     printf("\nindex=%d \t %d \t %d", search(1), getByData(1)->info, getByIndex(search(1))->info);  //index=0 value=1
     printf("\nindex=%d \t %d \t %d", search(2), getByData(2)->info, getByIndex(search(2))->info);
     printf("\nindex=%d", search(3));
-    printf("\nindex=%d", search(5));   //-1 not found
+    printf("\n150 is at index=%d", search(150));   //-1 not found
 
 
     return 0;
