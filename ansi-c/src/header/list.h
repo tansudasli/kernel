ff86fb2e79eda5ab91e3ad4a6f0d29aca757ba36
tansudasli <tansudasli@gmail.com>
2021-01-22 00:46:28 +0300

    minor update

--- ansi-c/src/header/list.h
+++ ansi-c/src/header/list.h
@@ -23,19 +23,15 @@ NODE * getByIndex(int);         //return Nth node by node-index
 NODE * getByData(int);          //return Nth node by data,
 
 NODE * append(int);             //creates 1st node, or appends to the last node
-/**
- * insert at index (random)
- * search w/ data, and append to it (before/after). kinda sorting
- */
 NODE * insertAt(int, int);      //insert at 1st, middle, end
 int deleteAt(int);              //delete at 1st, middle, last, only
 
 NODE * reverse(NODE *);
 
-void display(NODE *);          //prints
-int count();                   //node count
+void display(NODE *);           //prints
+int count();                    //node count
 
-int search(int);               //return node-index by data
+int search(int);                //return node-index by data
 
 #ifndef ANSI_C_LIST_H
 #define ANSI_C_LIST_H
