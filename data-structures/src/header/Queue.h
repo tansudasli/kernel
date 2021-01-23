0c11fb7f710d133739bf09c5342388f896e66510
tansudasli <tansudasli@gmail.com>
2021-01-23 20:05:51 +0300

    Initialize

--- data-structures/src/header/Queue.h
+++ data-structures/src/header/Queue.h
@@ -1,3 +1,46 @@
+/**
+ * Works as FIFO.
+ *
+ * Basic logic behind queue.
+ *
+ *        front                              rear
+ *     info|(addr1) ...  info|(addr2) ...  info|add3
+ *
+ * @details Insert (enqueue) to end, Remove (dequeue) from start. Insert/Delete between nodes not allowed.
+ * @author tansudasli
+ */
+
+/**
+
+ */
+typedef struct node NODE;
+struct node {
+    int info;
+
+    NODE *link;
+};
+
+NODE *front, *rear;                    //1st and last node
+
+//interfaces
+static NODE * create();                //creates start node,
+static NODE * initialize(int);         //creates start node and adds to 1st node
+
+NODE * getFront();                //returns 1st node
+NODE * getRear();                 //returns last node
+bool isEmpty();
+
+NODE * enqueue(int);            //inserts at 1st node
+NODE * dequeue();               //removes last node
+NODE * peek();                  //returns 1st node without dequeue
+
+void display();                 //prints
+int size();                     //node count
+
+#ifndef ANSI_C_STACK_H
+#define ANSI_C_STACK_H
+
+#endif //ANSI_C_STACK_H
 
 
 #ifndef ANSI_C_QUEUE_H
--- data-structures/src/header/Stack.h
+++ data-structures/src/header/Stack.h
@@ -1,4 +1,38 @@
+/**
+ * Works as LIFO.
+ *
+ * Basic logic behind stack.
+ *
+ *      top             addr2             addr3
+ *     (addr2) ...  info|(addr3) ...  info|add3
+ *
+ * @details Only insert or remove the 1st node. Insert/Delete between nodes not allowed.
+ * @author tansudasli
+ */
 
+
+typedef struct node NODE;
+struct node {
+    int info;
+
+    NODE *link;
+};
+
+NODE *top;                             //1st node
+
+//interfaces
+static NODE * create();                //creates start node,
+static NODE * initialize(int);         //creates start node and adds to 1st node
+
+NODE * getTop();                //returns 1st node
+bool isEmpty();
+
+void push(int);                 //inserts at 1st node
+NODE * pop();                   //removes 1st node
+NODE * peek();                  //returns 1st node without pop
+
+void display();                 //prints
+int size();                     //node count
 
 #ifndef ANSI_C_STACK_H
 #define ANSI_C_STACK_H
