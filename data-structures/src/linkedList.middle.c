c754aa634d5850a467d27a7ff1e63d0808aa8285
tansudasli <tansudasli@gmail.com>
2021-02-02 20:25:26 +0300

    Additional testcases added

--- data-structures/src/linkedList.middle.c
+++ data-structures/src/linkedList.middle.c
@@ -14,10 +14,11 @@ int main (int argc, char **argv) {
     //add to last
     //created as sorted intentionally!
     //even or odd is important
-    int l = 6;
-    DATA n[] = {5, 8, 10, 20, 31, 57};
+    int l = 11;
+//    DATA n[] = {5, 8, 10, 20, 31, 57};
+    DATA n[] = {5, 6, 8, 10, 12, 20, 21, 22, 31, 57, 72};
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < l; i++)
        append(n[i]);
 
     //
@@ -25,19 +26,19 @@ int main (int argc, char **argv) {
 
     //check middle func : start .....to.....
     printf("\nstart .....to.....");
-    NODE *m = getMiddle(getFirst(), getLast());
+    NODE *m = getMiddle(getFirst()->next, getLast());
     NODE *last = getLast();
     for (int i = 0; i < l; i++) {
-       printf("\n%d....m....%d=> m=%d", getFirst()->data.info, last->data.info, m->data.info);
+       printf("\n%d....m....%d=> m=%d", getFirst()->next->data.info, last->data.info, m->data.info);
 
        last = m;
-       m =  getMiddle(getFirst(), m);
+       m =  getMiddle(getFirst()->next, m);
     }
 
     printf("\n\n");
     printf(" .....to.....last");
-    m = getMiddle(getFirst(), getLast());
-    NODE *first = getFirst();
+    m = getMiddle(getFirst()->next, getLast());
+    NODE *first = getFirst()->next;
     for (int i = 0; i < l; i++) {
         printf("\n%d....m....%d=> m=%d",  first->data.info, getLast()->data.info, m->data.info);
 
