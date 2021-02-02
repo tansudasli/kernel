f2b7fb76fc837b6bb7e1ca5dbc89601134bb9e5d
tansudasli <tansudasli@gmail.com>
2021-02-02 13:45:44 +0300

    Additional todos added

--- data-structures/src/header/ArrayList.h
+++ data-structures/src/header/ArrayList.h
@@ -42,10 +42,10 @@ NODE * getFirst();                   //O(1) - returns first node
 NODE * getLast();                    //O(1) - returns last node
 NODE * getMiddle(NODE *b, NODE *e);  //O(logn) - returns in the middle node
 
-NODE * search(DATA s);          //O(n) - search                                //todo: data dependent comparison!
+NODE * search(DATA s);          //O(n) - search                                //data dependent comparison!
 NODE * search2(int s);          //search by int in DATA struct                 //todo: impl.
 NODE * search3(char* s);        //search by string in DATA struct              //todo: impl.
-NODE * binarySearch(DATA s);    //O(logn) - search by splitting into middles   //todo: data dependent comparison!
+NODE * binarySearch(DATA s);    //O(logn) - search by splitting into middles   //data dependent comparison!
 
 void append(DATA d);                            //O(1) - appends to the last
 void attach(DATA d);                            //O(1) -  inserts at the beginning
@@ -53,14 +53,15 @@ void insert(DATA s, POSITION pos, DATA d);      //O(n) - insert between first an
 void replace(DATA s, DATA r);                   //O(n) - replace s with r          //todo: impl.
 
 int delete(DATA s);             //O(n) - delete at 1st, middle, last, only  //todo: impl.
-int clean();                    //deletes all records (free heap)    //todo: add a method to delete all records
+int clean();                    //deletes all records (free heap)           //todo: add a method to delete all records
+                                                                            //todo: add onexit signal
 
 NODE * reverse();               //O(n) - reverse (1st becomes last etc..)   //todo: impl.
 
-void display();                 //O(n) - prints                  //todo: data dependent printf!
+void display();                 //O(n) - prints                                //data dependent printf!
 void stats();                   //O(n) - print some stat data
 int size();                     //O(1) - node count
-bool isSorted();                //O(1) - default not sorted      //todo: impl.
+bool isSorted();                //O(1) - default not sorted                 //todo: impl.
 
 #ifndef ANSI_C_LIST_H
 #define ANSI_C_LIST_H
