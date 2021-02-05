1ec3133121de5e97edd5474c04f74036e81011db
tansudasli <tansudasli@gmail.com>
2021-02-05 14:38:05 +0300

    Added more documentation

--- data-structures/src/collection/Stack.c
+++ data-structures/src/collection/Stack.c
@@ -4,6 +4,12 @@
 
 #include "../header/Stack.h"
 
+/**
+ * If top is Null, then creates 1st node and pushes. Otherwise, pushes just as top.<br>
+ * No need to use init(), or create(). Very lightweight and simple.<br>
+ *
+ * @param d: DATA new node
+ */
 void push(DATA d) {
 
     NODE *p = (NODE *) calloc(1, sizeof(NODE));
@@ -20,6 +26,11 @@ void push(DATA d) {
     top = p;
 }
 
+/**
+ * Gets last in node, then updates the remaining as top.
+ *
+ * @return : top node
+ */
 NODE * pop() {
     //get top, adjust and return
     if (!top) {
@@ -36,11 +47,21 @@ NODE * pop() {
     return p;
 }
 
+/**
+ * Get last in node, but do not update top. Good for checking, comparing etc..<br>
+ * 
+ * @return : top node
+ */
 NODE * peek() {
 
     return top;
 }
 
+/**
+ * Check stack has nodes or not.<br>
+ *
+ * @return true, if empty or otherwise false.
+ */
 bool isEmpty() {
     
    return !top ? true : false;
