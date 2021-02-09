c2cc6267c3282a252918ed98f5f2848eab347c6b
tansudasli <tansudasli@gmail.com>
2021-02-09 22:23:22 +0300

    Minor updates

--- ansi-c/src/pointerToPointer.c
+++ ansi-c/src/pointerToPointer.c
@@ -4,9 +4,9 @@
 //gcc -o out/pointerToPointer src/pointerToPointer.c  && out/pointerToPointer
 int main(int argc, char **argv) {
 
-    int k[] = {1, 2, 3, 4};
-    int l[] = {2, 4, 6, 8};
-    int m[] = {1, 8, 27, 64};
+    int k[] = {1, 2, 3, 4, '\0'};   //if we dont put \0, below while (*o[i] != '\0') does not work properly!!!!
+    int l[] = {2, 4, 6, 8, '\0'};
+    int m[] = {1, 8, 27, 64, '\0'};
 
 
     int *o[3];   //if we define int **o, then we can not initialize properly.
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
     o[1] = l;  //2 4 6 8 1 2 3 4 -515139872 32766 -1857290203 -261192148
     o[2] = m;  //1 8 27 64 2 4 6 8 1 2 3 4
 
-    //below is also works like above.
+    //below is also works like above. but we cannot use pointer arithmetic !!!
     // on the other side, C holds [][] as 1 2 3 4 2 4 6 8 1 8 27 64 1 8 27 64  format. not below!
 //    int o[3][4]  = {{1, 2, 3, 4},
 //                    {2, 4, 6, 8},
@@ -25,8 +25,10 @@ int main(int argc, char **argv) {
 //                    };
 
     for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 12; j++)
-            printf("%d ", o[i][j]);
+//        for (int j = 0; j < 12; j++)
+           while (*o[i] != '\0')
+            printf("%d ", *o[i]++);
+
 
         printf("\n");
     }
