eb01b4246600852642f7a8c8ae2199d4035c132f
tansudasli <tansudasli@gmail.com>
2021-02-13 23:51:33 +0300

    More clarification

--- ansi-c/src/structs.c
+++ ansi-c/src/structs.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
-#include "header/structs.h"
+#include <string.h>
 
-//  gcc src/structs.c src/utility/initialize.c -o out/structs  && out/structs
+#ifndef NUMBER_OF_CDS
+#define NUMBER_OF_CDS 2
+#endif //NUMBER_OF_CDS
+
+typedef enum score {
+    TERRIBLE, BAD, AVERAGE, GOOD, EXCELLENT
+} SCORE;
+
+typedef struct cd {
+    char title[50];
+    char artist[50];
+    int trackCount;
+    SCORE rating;
+} CD; //So we can use like native int/char type...
+
+CD cdDataset[NUMBER_OF_CDS];
+
+//gcc src/structs.c -o out/structs  && out/structs
 int main(int argc, char **argv) {
 
-    initializeDataset();
+    //initialize
+    strcpy(cdDataset[0].title, "Once upon a time");
+    strcpy(cdDataset[0].artist, "Ali Veli");
+    cdDataset[0].trackCount = 10;
+    cdDataset->rating = TERRIBLE;
+
+    strcpy(cdDataset[1].title, "Just run forest");
+    strcpy(cdDataset[1].artist, "Mali Dali");
+    cdDataset[1].trackCount = 1;
+    cdDataset->rating = AVERAGE;
 
     printf("-------Access w/ values--------\n");
     printf("Title \t\t\t| Rating\n");
